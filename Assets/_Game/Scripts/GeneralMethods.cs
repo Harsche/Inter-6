e2f5e5c60675d6a9592b22d81edf6333b25c1d5f
Harsche <henrique_harsche@hotmail.com>
2022-09-21 15:42:43 -0300

    Arrumei alguns scripts e a iluminação

---
+++ Assets/_Game/Scripts/GeneralMethods.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(fileName = "GeneralMethods", menuName = "General Methods", order = 0)]
+public class GeneralMethods : ScriptableObject{
+    public void ChangeScene(string sceneName){
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void CloseGame(){
+        Application.Quit();
+    }
+}
