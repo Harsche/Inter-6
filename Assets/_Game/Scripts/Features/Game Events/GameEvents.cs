921e61c2a0d3038acae3cf0206ab0b5ea6f55c16
Harsche <henrique_harsche@hotmail.com>
2022-10-03 02:56:22 -0300

    Adicionei os scripts de eventos do jogo

---
+++ Assets/_Game/Scripts/Features/Game Events/GameEvents.cs
@@ -0,0 +1,40 @@
+﻿using GameDialogs;
+using Inventory;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(menuName = "Game Events")]
+public class GameEvents : ScriptableObject{
+    public void TriggerDialog(TextAsset dialogJson){
+        Dialog.Instance.TriggerDialog(dialogJson);
+    }
+
+    public void CollectItem(Item item){
+        GameInventory.Instance.AddItem(item);
+    }
+
+    // public void PlaySound(SoundPlayer soundPlayer){
+    //     soundPlayer.PlaySound();
+    // }
+
+    // public void ViewItem(Item item){
+    //     ItemDisplay.Instance.DisplayItem(item);
+    // }
+
+    public void ActivateOnDialogEnd(GameObject gameObject){
+        Dialog.Instance.OnDialogEnd += () =>  gameObject.SetActive(true);
+    }
+
+    public void LoadScene(string sceneName){
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // public void TeleportPlayer(Transform teleportPosition){
+    //     PlayerInteraction.Instance.PlayerRigidbody.position = teleportPosition.position;
+    // }
+
+    public void DontDestroy(GameObject gameObject){
+        DontDestroyOnLoad(gameObject);
+        gameObject.tag = "DontDestroy";
+    }
+}
---
+++ Assets/_Game/Scripts/Features/Game Events/RequireItemEventTrigger.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using Inventory;
+
+public class RequireItemEventTrigger : MonoBehaviour, IInteractable{
+    [SerializeField] private bool disableObject;
+    [SerializeField] private bool consumeItem = true;
+    [SerializeField] private Item requiredItem;
+    [SerializeField] private UnityEvent onUseItem;
+    [SerializeField] private UnityEvent missingItem;
+    [SerializeField] private GameObject outline;
+
+    private bool selected;
+
+    private void Awake(){
+        gameObject.layer = LayerMask.NameToLayer("Interactable");
+    }
+
+    public bool Interact(){
+        bool interacted = consumeItem
+            ? GameInventory.Instance.RemoveItem(requiredItem)
+            : GameInventory.Instance.CheckItem(requiredItem);
+
+        if (!interacted){
+            missingItem?.Invoke();
+            return false;
+        }
+
+        onUseItem?.Invoke();
+        if (disableObject) gameObject.SetActive(false);
+        return true;
+    }
+
+    public void ToggleHighlight(bool active){
+        outline.SetActive(active);
+    }
+}
