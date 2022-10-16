bab6193b17d9641c1e2ec605aa50cd4ba7bec17d
Harsche <henrique_harsche@hotmail.com>
2022-10-16 11:07:16 -0300

    Ajustes na tela de título

---
+++ Assets/_Game/Scripts/UI/Main Menu/Intro.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Intro : MonoBehaviour{
+    [SerializeField] private VideoPlayer videoPlayer;
+    [SerializeField] private CanvasGroup canvasGroup;
+    [SerializeField] private float fadeDuration = 2f;
+    [SerializeField] private Ease fadeEase = Ease.Linear;
+
+    private void Awake(){
+        StartCoroutine(WaitForIntro());
+    }
+
+    private IEnumerator WaitForIntro(){
+        float introLength = (float) videoPlayer.clip.length;
+        yield return new WaitForSeconds(introLength);
+        videoPlayer.gameObject.SetActive(false);
+        canvasGroup.DOFade(0f, fadeDuration)
+            .SetEase(fadeEase)
+            .OnComplete(() => canvasGroup.blocksRaycasts = false);
+    }
+}
