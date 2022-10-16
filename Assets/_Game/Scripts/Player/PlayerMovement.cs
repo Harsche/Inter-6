62146a9bf4cde329e6b2e69ce88fea9a4fe67328
Alecsia Fernandes <alecsiafernandes@gmail.com>
2022-10-16 16:28:32 -0300

    Reorganizei scripts de movimentação e camera.

--- Assets/_Game/Scripts/Player/PlayerMovement.cs
+++ Assets/_Game/Scripts/Player/PlayerMovement.cs
@@ -1,21 +1,33 @@
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour{
+public class PlayerMovement : MonoBehaviour
+{
     private CharacterController controller;
     private Animator animator;
 
-    [SerializeField] GameObject head;
-    [SerializeField] float sensitivityCam;
+    [SerializeField] CameraMovement cameraRef;
     [SerializeField] float gravity;
     [SerializeField] float jumpForce;
 
     private Vector3 movement;
-    private float camY;
     private float moveY;
+
     public bool useStamina;
+
+    private bool isMoving;
+    private bool isCrawl;
+    private bool isKick;
+    private bool isRun;
+    private bool isSlowly;
+    private bool isJump;
+
+    private float isMovingH;
+    private float isMovingV;
+
     public bool stopInput;
 
-    void Start(){
+    void Start()
+    {
         controller = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
 
@@ -25,34 +37,44 @@ public class PlayerMovement : MonoBehaviour{
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    void Update(){
-        if (Input.GetButton("Horizontal") ||
-            Input.GetButton("Vertical")) //Definindo variacoes na velocidade de movimento
-        {
-            if (animator.GetBool("isCrawling"))
-            {
-                animator.SetFloat("Velocity", 3f);
-            }
-            else if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
-            {
-                animator.SetFloat("Velocity", 6.5f);
-                useStamina = true;
-            }
-            else if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
-            {
-                animator.SetFloat("Velocity", 1.8f);
-                useStamina = false;
-            }
-            else
-            {
-                animator.SetFloat("Velocity", 4f);
-                useStamina = false;
-            }
-        } else animator.SetFloat("Velocity", 0f);
+    void Update()
+    {
+        GetInput();
 
-        movement = Input.GetAxis("Vertical") * transform.forward; //Associando valores de movimentacao
+        if (isMoving) PlayerVelocity();
+        else animator.SetFloat("Velocity", 0f);
 
-        movement += Input.GetAxis("Horizontal") * transform.right;
+        Crawl();
+        Kick();
+
+        PlayerMove();
+
+        cameraRef.CameraMove();
+    }
+
+    void GetInput() //Setando inputs
+    {
+        if (stopInput) return;
+
+        cameraRef.valueCamY = Input.GetAxis("Mouse Y");
+        cameraRef.valueCamX = Input.GetAxis("Mouse X");
+
+        isMovingH = Input.GetAxis("Horizontal");
+        isMovingV = Input.GetAxis("Vertical");
+
+        isMoving = Input.GetButton("Horizontal") || Input.GetButton("Vertical");
+        isCrawl = Input.GetKey(KeyCode.C);
+        isKick = Input.GetKeyDown(KeyCode.F);
+        isRun = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+        isSlowly = Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl);
+        isJump = Input.GetButton("Jump");
+    }
+
+    void PlayerMove() //Associando valores de movimentacao
+    {
+        movement = isMovingV * transform.forward; 
+
+        movement += isMovingH * transform.right;
 
         movement *= animator.GetFloat("Velocity");
 
@@ -65,21 +87,45 @@ public class PlayerMovement : MonoBehaviour{
         movement *= Time.deltaTime;
 
         controller.Move(movement);
-
-        Crawl();
-        Kick();
-        CameraMovement();
     }
 
-    void Crawl(){
-        if (Input.GetKeyDown(KeyCode.C)){
+    void PlayerVelocity() //Definindo variacoes na velocidade de movimento
+    {
+        if (!isMoving) return;
+
+        if (isCrawl)
+        {
+            animator.SetFloat("Velocity", 3f);
+        }
+        else if (isRun)
+        {
+            animator.SetFloat("Velocity", 6.5f);
+            useStamina = true;
+        }
+        else if (isSlowly)
+        {
+            animator.SetFloat("Velocity", 1.8f);
+        }
+        else //velocidade normal de caminhada
+        {
+            animator.SetFloat("Velocity", 4f);
+        }
+    }
+
+    void Crawl()
+    {
+        if (isCrawl)
+        {
             animator.SetBool("isCrawling", true);
+
             controller.height = 0.8f;
             controller.center = new Vector3(0, 0.37f, 0);
         }
 
-        if (Input.GetKeyUp(KeyCode.C)){
+        if (!isCrawl)
+        {
             animator.SetBool("isCrawling", false);
+
             controller.height = 1.8f;
             controller.center = new Vector3(0, 0.91f, 0);
         }
@@ -87,22 +133,23 @@ public class PlayerMovement : MonoBehaviour{
 
     void Kick()
     {
-        if(Input.GetKeyDown(KeyCode.F))
+        if(isKick)
         {
             animator.SetBool("isKicking", true);
         }
 
-        if (Input.GetKeyUp(KeyCode.F))
+        if (!isKick)
         {
             animator.SetBool("isKicking", false);
         }
     }
 
-    float Jump(){
+    float Jump()
+    {
         if (controller.isGrounded){
             moveY = 0f;
 
-            if (Input.GetButton("Jump")){
+            if (isJump && !isCrawl){
                 moveY += jumpForce;
                 animator.SetBool("isJumping", true);
             }
@@ -112,24 +159,15 @@ public class PlayerMovement : MonoBehaviour{
         return moveY;
     }
 
-    void CameraMovement(){
-        float finalSensitivity = sensitivityCam * (1 + PlayerPrefs.GetFloat("CameraSensitivity"));
-        transform.Rotate(0, Input.GetAxis("Mouse X") * finalSensitivity * Time.deltaTime,
-            0); //Movimento de camera horizontal
-
-        camY += -Input.GetAxis("Mouse Y") * finalSensitivity * Time.deltaTime; //Movimento da camera vertical
-        camY = Mathf.Clamp(camY, -75, 75);
-        head.transform.localEulerAngles = new Vector3(camY, 0, 0);
-    }
-
-    float Gravity(){
+    float Gravity()
+    {
         moveY += gravity;
         return moveY;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.tag == ("Enemy"))
+        if(collision.gameObject.CompareTag("Enemy"))
         {
             print("chutou");
         }
