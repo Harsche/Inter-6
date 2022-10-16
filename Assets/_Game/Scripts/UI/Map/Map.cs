1f4b67b7544a0e27796037a2b9ae904c36697bef
Harsche <henrique_harsche@hotmail.com>
2022-10-16 06:44:53 -0300

    Backup commit

--- Assets/_Game/Scripts/UI/Map/Map.cs
+++ Assets/_Game/Scripts/UI/Map/Map.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 public class Map : MonoBehaviour{
     [SerializeField] private RectTransform mapSpaceTransform;
     [SerializeField] private BoxCollider gameSpaceBoxCollider;
+    [SerializeField] private Transform playerIcon;
 
     private Bounds gameSpaceBounds;
     private Vector2 mapSpaceSize;
+    private Vector2 halfMapSpaceSize;
 
     public static Map Instance{ get; private set; }
 
@@ -20,22 +22,29 @@ public class Map : MonoBehaviour{
 
         gameSpaceBounds = gameSpaceBoxCollider.bounds;
         mapSpaceSize = mapSpaceTransform.sizeDelta;
+        halfMapSpaceSize = mapSpaceSize / 2;
     }
 
     public void UpdateMap(){
         // Calculate Player Position
-
         Vector3 playerPosition = Player.Instance.transform.position;
 
         var inMinMaxX = new Vector2(gameSpaceBounds.min.x, gameSpaceBounds.max.x);
         var inMinMaxY = new Vector2(gameSpaceBounds.min.x, gameSpaceBounds.max.x);
         var inMinMaxZ = new Vector2(gameSpaceBounds.min.x, gameSpaceBounds.max.x);
 
-        var outMinMaxX = new Vector2(-mapSpaceSize.x / 2, mapSpaceSize.x / 2);
-        var outMinMaxY = new Vector2(-mapSpaceSize.y / 2, mapSpaceSize.y / 2);
+        var outMinMaxX = new Vector2(-halfMapSpaceSize.x, halfMapSpaceSize.x);
+        var outMinMaxY = new Vector2(-halfMapSpaceSize.y, halfMapSpaceSize.y);
 
         var playerMapPosition = new Vector2(
             UtilityMethods.RemapClamped(inMinMaxX, outMinMaxX, playerPosition.x),
             UtilityMethods.RemapClamped(inMinMaxY, outMinMaxY, playerPosition.y)
         );
+
+        Vector2 finalMapPosition = -halfMapSpaceSize + playerMapPosition;
+        playerIcon.position = finalMapPosition;
     }
+    
+    
+}
+    
