9a9f01e2b2d94f5ee8f3945e59300978ffaadd7a
Harsche <henrique_harsche@hotmail.com>
2022-11-16 03:32:37 -0300

    Add os scripts restantes do treinamento

---
+++ Assets/_Game/Scripts/Features/Specific/JumpTraining.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class JumpTraining : MonoBehaviour{
+    [SerializeField] private Transform checkPosition;
+    [SerializeField] private int jumpsRequired;
+    [SerializeField] private int jumpCount;
+    [SerializeField] private float cooldownTime = 0.2f;
+    [SerializeField] private UnityEvent onExecuteAllJumps;
+    private bool canAddJump;
+
+    private Coroutine cooldownCoroutine;
+
+    private void Awake(){
+        canAddJump = true;
+    }
+
+    private void OnTriggerExit(Collider other){
+        if (other.gameObject.name != "KickCollider") return;
+        HandleJump(other.transform.position);
+    }
+
+    private void HandleJump(Vector3 collisionPosition){
+        if (!canAddJump || jumpCount >= jumpsRequired || collisionPosition.y <= checkPosition.position.y) return;
+        jumpCount++;
+        canAddJump = false;
+        if (cooldownCoroutine != null) StopCoroutine(cooldownCoroutine);
+        cooldownCoroutine = StartCoroutine(CooldownCoroutine());
+        if (jumpCount == jumpsRequired) onExecuteAllJumps?.Invoke();
+    }
+
+    private IEnumerator CooldownCoroutine(){
+        yield return new WaitForSeconds(cooldownTime);
+        canAddJump = true;
+    }
+}
---
+++ Assets/_Game/Scripts/Features/Specific/KickTraining.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class KickTraining : MonoBehaviour{
+    [SerializeField] private int kicksRequired;
+    [SerializeField] private int kickCount;
+    [SerializeField] private float cooldownTime = 0.2f;
+    [SerializeField] private UnityEvent onExecuteAllKicks;
+    private bool canAddKick;
+
+    private Coroutine cooldownCoroutine;
+
+    private void Awake(){
+        canAddKick = true;
+    }
+
+    private void OnCollisionEnter(Collision other){
+        if (other.gameObject.name != "KickCollider") return;
+        HandleKick();
+    }
+    
+    private void HandleKick(){
+        if (!canAddKick || kickCount >= kicksRequired) return;
+        kickCount++;
+        canAddKick = false;
+        if (cooldownCoroutine != null) StopCoroutine(cooldownCoroutine);
+        cooldownCoroutine = StartCoroutine(CooldownCoroutine());
+        if (kickCount == kicksRequired) onExecuteAllKicks?.Invoke();
+    }
+    
+    private IEnumerator CooldownCoroutine(){
+        yield return new WaitForSeconds(cooldownTime);
+        canAddKick = true;
+    }
+}
